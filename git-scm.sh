0d9813431c98b56d801ba3a14795996e77adb202
antonyxu <xujie.law@qq.com>
2022-09-09 15:41:53 -0400

    fix bugs

--- git-scm.sh
+++ git-scm.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-#
-#if [ ! "$(git pull)"=="Already up to date.\n" ]; then
+
 echo "Find update!"
 git reset --hard
 git pull
 sh upload_unmask.sh
 rsync ./* ../docker -avu
-#fi
 
 # docker pull httpd:alpine
 # htpasswd -nbBC 10 USER topsecret
