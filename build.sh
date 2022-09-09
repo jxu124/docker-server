0d64630ebcd5cee0116a83278dbafe8891d4968d
antonyxu <xujie.law@qq.com>
2022-09-09 08:43:04 -0400

    fix bugs

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd transmission && docker build . -t antonyxu/transmission && cd ..
-cd v2ray && docker build . -t antonyxu/v2ray && cd ..
-cd webdav && docker build . -t antonyxu/webdav && cd ..
